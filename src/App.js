1c2f1ae3347fc7a2c1fb0b493451fab22a76c5f6
sam1610 <samir.amri@gmail.com>
2023-03-08 08:24:39 +0300

    update Route

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import Navbar from "./Components/Navbar";
 import  {Work}   from "./Components/Work";
 import  {Home}   from "./Components/Home";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import RootLayout from './Components/RootLayout';
 
-
+const router=createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<RootLayout />}>
+			<Route index element={<Home />}/>
+			<Route path="work" element={<Work />}/>
+		</Route>
+	)
+)
 function App() {
 	return (
 		<>
-		<Navbar/>
-		<Router>
-			<Routes>
-				<Route  path="/" element={ <Home />} />
-				<Route  path="/work" element={ <Work />} />
-			</Routes>
-
-		</Router>
+		{/* <Navbar/> */}
+		<RouterProvider router={router} />
 		</>
 	);
 }
---
+++ src/Components/RootLayout.js
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/main.css";
+import { Outlet , NavLink  } from "react-router-dom";
+
+function RootLayout() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+        <>
+       
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef} onClick={showNavbar}>
+				<NavLink to="/">Home </NavLink>
+				<NavLink to="/work">My work</NavLink>
+				<NavLink to="/blog">Blog</NavLink>
+				<NavLink to="/me">about me</NavLink>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+        <main>
+            <Outlet />
+        </main>
+        </>
+    
+	);
+}
+
+export default RootLayout;
